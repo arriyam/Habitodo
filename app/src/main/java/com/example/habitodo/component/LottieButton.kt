3e1b0a561fc55be8ac489c3a00d128f98b8711d3
Thushen Mohanarajah <thushenarriyam@gmail.com>
2024-09-28 18:43:01 -0400

    Small changes to LottieButton

--- app/src/main/java/com/example/habitodo/component/LottieButton.kt
+++ app/src/main/java/com/example/habitodo/component/LottieButton.kt
@@ -16,10 +16,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.Dp
 
 @Composable
 fun LottieButton(
     lottieRes: Int,
+    boxSize: Dp,
+    padding: Dp,
+    lottieSize : Dp,
     onClick: () -> Unit
 ) {
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottieRes)) // replace with your Lottie animation resource
@@ -33,18 +37,18 @@ fun LottieButton(
         // Use Box to make the Lottie animation clickable without feedback
         Box(
             modifier = Modifier
-                .size(80.dp) // Adjust size for your button
+                .size(boxSize) // Adjust size for your button
                 .clickable(
                     onClick = onClick, // Handle the click
                     indication = null, // Disable the click feedback
                     interactionSource = remember { MutableInteractionSource() } // Required when indication is null
                 )
-                .padding(8.dp) // Optional: add some padding around the Lottie animation
+                .padding(padding) // Optional: add some padding around the Lottie animation
         ) {
             LottieAnimation(
                 composition = composition,
                 iterations = LottieConstants.IterateForever,
-                modifier = Modifier.size(75.dp) // Adjust the size of the Lottie animation
+                modifier = Modifier.size(lottieSize) // Adjust the size of the Lottie animation
             )
         }
     }
